fd25daa95f6e7231f17524ea45bc20afcbcd7eb5
charan <2000030176@kluniversity.in>
2021-09-19 18:04:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {ComputerScience, Electrical, Mechanical, BioTech} from './departments'
+import {useState} from 'react'
+import {Button} from '@material-ui/core'
 
-function App() {
+ const s1 = {
+    border: '1px solid red',
+    width: '800px',
+    boxShadow: '2px 3px 5px black',
+    color: 'green',
+    margin: 'auto',
+    textAlign: 'center',
+    textShadow: '1px 1px 2px light green, 1px 2px 1px yellow'
+  }
+
+  const s2 = {
+    margin: 20
+  }
+
+const Skill3 = () => {
+  const [dept, setDept] = useState(BioTech)
+
+  const changeView = (dep) => {
+    setDept (dep);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style = {s1}>
+
+    <div>
+    <Button style = {s2} variant="outlined" color="secondary" onClick = {() => changeView(ComputerScience)}> CSE </Button>
+    <Button style = {s2} variant="outlined" color="secondary" onClick = {() => changeView(Electrical)}> ECE </Button>
+    <Button style = {s2} variant="outlined" color="secondary" onClick = {() => changeView(Mechanical)}> ME </Button>
+    <Button style = {s2} variant="outlined" color="secondary" onClick = {() => changeView(BioTech)}> BT </Button>
     </div>
-  );
+
+      <p> WELCOME TO SKILL MATERIAL UI</p>
+      <img src = {dept[0].imageUrl} alt = "Bio Tech" width = '760' />
+      <p> {dept[0].displayName} </p>
+      <p> {dept[0].description} </p>
+    </div>
+    )
 }
 
-export default App;
+export default Skill3
---
+++ src/departments.js
@@ -0,0 +1,41 @@
+export const ComputerScience = [
+    {
+      uuid: 1,
+      displayName: "Computer Science Department",
+      imageUrl: "assets/CSE.jpg",
+      description:
+        "Department of Computer Science & Engineering offers undergraduate, post graduate and Doctoral programs with a flexible and industry curriculum and innovative pedagogy that is very dynamic in capturing the latest trends in the industry and the job market. The emphasis will be on making the student ready to be fit for the immediate industry needs",
+    
+    }
+  ]
+  
+  export const Electrical = [  {
+      uuid: 2,
+      displayName: "Electrical Department",
+      imageUrl: "assets/EEE.jpg",
+      description:
+        "Welcome to the Department of Electrical & Electronics Engineering at Koneru Lakshmaiah Education Foundation. We started our journey in the year of 1995. Over the last few decades, we have grown our expertise and competence in the core Electrical & Electronics Engineering curriculum and research. We have a strong undergraduate program in Electrical & Electronics engineering, including B. Tech, M. Tech. dual degree program. At the postgraduate level, we offer M. Tech.,& M. Tech., by Research and PhD degrees. The sanctioned students’ strengths of B. Tech., M. Tech., and M. Tech., by research are respectively 60, 36 and 10 per year. At present, the department has 167 PhD scholars. Several sponsored candidates from industries and research laboratories carry out their postgraduate studies in our department.",
+     
+    }
+  
+  
+  ]
+  export const Mechanical = [ {
+      uuid: 3,
+      displayName: "Mechanical Department",
+      imageUrl: "assets/ME.jpg",
+      description:
+        "Established in1980, the Mechanical Engineering is one of the most versatile and broadest fields of engineering. The key areas of Mechanical Engineering like ‘Product Design’, ‘Smart Manufacturing & Automation’ and ‘Automobile Electronics’ have opened vast opportunities in industries allied to Automobiles, Aerospace, Renewable Energy, Manufacturing, Oil & Gas Production and Subsea technologies in renowned MNC’s and Government companies. Especially, with emerging growth in automation, along with boys, girl students also find larger and better scope in Mechanical engineering sector.",
+      
+    },
+  ]
+  
+  export const BioTech = [
+    {
+      uuid: 4,
+      displayName: "BioTech Department",
+      imageUrl: "assets/BT.jpg",
+      description:"The strength of the department is the faculty drawn from reputed national and international academic & research organizations. The faculty has more than 15 years of teaching experience with good industrial exposure. Faculty with postdoctoral research experience is feathers in the cap of the department. The faculty are extensively involved in quality research & development and in the last 3 years has acquired projects worth more than 3 crores rupees in the areas of biofuels, biomedical research, genomics and proteomics, bioinformatics and food biotechnology funded by prestigious research organizations like DBT, DST, UGC and ICMR. The department also has women scientists working under DST funded projects. The Department has filed patents with IPO in collaboration with industry. The biotechnology department at KL University has 7 well-furnished and fully equipped state-of-the-art laboratories, 4 research centers along with own computer center with latest molecular modeling and drug design software purchased from Schrodinger LLC, USA. As a part of student centric learning development various measures and initiatives are taken to improve the skills of students. These include exposure to guest lectures, industrial training and tours, communication and soft skills, mini-projects, paper presentations in national level paper contests, classroom seminars, placement opportunities, academic & career counseling, certificate courses, live projects in industry and exposure to journals etc. Another area of concentration for the faculty is research consultancy. The department has signed MoUs with Sneha Biotech, Vijayawada and NRI Academy of Medical Sciences, Guntur for faculty and student training and collaborative research. The department of biotechnology is also in collaboration with the Indian Institute of Science, Bengaluru and University of Oxford, UK for initiating collaborative research in biomedical sciences.",
+     
+    },
+  ];
